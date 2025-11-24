bee9b5e1b3bfd333c9c6758bd65770981bb8c284
yashSkillminds <yash@skillminds.in>
2025-11-24 12:19:27 +0530

    fix: Correct dialect configuration in database connection setup

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -8,8 +8,8 @@ export const sequelize = new Sequelize(
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB,
-    // dialect: "mysql",
+    // dialect: process.env.DB,
+    dialect: 'mysql',
   },
 );
 
