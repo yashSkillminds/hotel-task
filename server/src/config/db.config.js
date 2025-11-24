a97c5d8972c19245eeef8d17f45e6e38a8384de8
yashSkillminds <yash@skillminds.in>
2025-11-24 15:36:14 +0530

    feat: Update .gitignore to exclude PEM files and refactor constants to use Object.freeze for immutability

--- server/src/config/db.config.js
+++ server/src/config/db.config.js
@@ -1,17 +1,30 @@
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';
+import fs from 'fs';
 
 // const sequelize = new Sequelize("mysql:memory");
-export const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    // dialect: process.env.DB,
-    dialect: 'mysql',
+export const sequelize = new Sequelize(process.env.DB_URI, {
+  dialect: 'mysql',
+  // dialect: process.env.DB,
+  dialectOptions: {
+    ssl: {
+      ca: fs.readFileSync(process.env.AIVEN_MYSQL_CA_PATH || './ca.pem'),
+      rejectUnauthorized: true,
+      require: true,
+    },
   },
-);
+});
+
+// export const sequelize = new Sequelize(
+//   process.env.DB_NAME,
+//   process.env.DB_USER,
+//   process.env.DB_PASS,
+//   {
+//     host: process.env.DB_HOST,
+//     // dialect: process.env.DB,
+//     dialect: 'mysql',
+//   },
+// );
 
 export const connectDB = async () => {
   try {
--- server/src/constants/constants.js
+++ server/src/constants/constants.js
@@ -1,39 +1,39 @@
-export const userRoles = {
+export const userRoles = Object.freeze({
   user: 'user',
   admin: 'admin',
-};
+});
 
-export const modelNames = {
+export const modelNames = Object.freeze({
   user: 'User',
   booking: 'Booking',
   hotel: 'Hotel',
   payment: 'Payment',
   room: 'Room',
-};
+});
 
-export const roomTypes = {
+export const roomTypes = Object.freeze({
   single: 'single',
   double: 'double',
   suite: 'suite',
-};
+});
 
-export const bookingStatus = {
+export const bookingStatus = Object.freeze({
   booked: 'booked',
   cancelled: 'cancelled',
-};
+});
 
-export const paymentStatus = {
+export const paymentStatus = Object.freeze({
   pending: 'pending',
   completed: 'completed',
   refunded: 'refunded',
-};
+});
 
 export const SIZE_LIMIT = '16kb';
 
-export const cookieOptions = {
+export const cookieOptions = Object.freeze({
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-};
+});
 
 export const DEFAULT_PAGINATION_DATA = {
   page: 1,
